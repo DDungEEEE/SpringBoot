a79a72dadcdc35aa57ecb19ca2f0967ee9c07640
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-01-03 18:56:11 +0900

    Notebook push

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,13 +2,47 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value ="name", required = false, defaultValue = "없음") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-templete";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody//http Body에 이 데이터를 직접 전송
+    public String helloString(@RequestParam(value = "name", defaultValue = "안녕") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
