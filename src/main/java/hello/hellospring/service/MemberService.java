5e7555e53b80fc3462d855963ceca2ec07de1b29
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-01-05 15:42:37 +0900

    sign up update

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public Long join(Member member){
+
+        checkOverlapMember(member);
+
+        //존재 하지 않으면 넘어가고 존재하면 안에 메서드가 실행됨
+        // result.orElseGet() -> 값이 있으면 꺼내고 없으면 default값을 넣어서 꺼냄
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void checkOverlapMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+    return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
